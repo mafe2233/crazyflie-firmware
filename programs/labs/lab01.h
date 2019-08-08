379e460fe0cc999613d228541bf831b1dd79b1b4
mafe2233 <mafe2233@gmail.com>
2019-08-08 08:45:43 -0300

    Update

--- programs/labs/lab01.h
+++ programs/labs/lab01.h
@@ -2,16 +2,24 @@
 #include "crazyflie.h"
 
 // Define all LEDs as digital output objects
-
+DigitalOut led1(LED_BLUE_R,!false);
+DigitalOut led2(LED_RED_R,!false);
+char t;
 // Define all motors as PWM objects
 
 // Main program
 int main()
 {
     // Blink blue LED indicating inicialization (5 seconds)
-    
+    while(t<5)
+    {
+        led1 = !led1;
+        wait(0.1);
+
+
+    }
     // Turn on red LEDs indicating motors are armed
-    
+    led2 = !led2
     // Test all motors with different frequencies (to make different noises)
     
     // Turn off red LEDs indicating motors are disarmed
